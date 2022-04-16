67bed60ed112dae1a9fbebfdd6841a1d07e9293b
Oliver Giles <ogiles@email.com>
2022-04-16 18:25:24 +0100

    added space for readability of output

--- prod-two.cpp
+++ prod-two.cpp
@@ -6,5 +6,5 @@ int main()
   std::cin >> v1 >> v2;
   std::cout << "The product of " << v1
             << " and " << v2 << std::endl;
-  std::cout << "Is" << v1 * v2 << std::endl;
+  std::cout << "Is " << v1 * v2 << std::endl;
 }
