9e065c11ecdb740f5e3e373cd63d3b4ca66f521e
Oliver Giles <ogiles@email.com>
2022-04-16 18:24:51 +0100

    added comments

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,8 +1,8 @@
-// Your First C++ Program
-
 #include <iostream>
 
 int main() {
+    // std::cout (terminal out) << writes in the string
     std::cout << "Hello World!";
+    // return 0 end the program with success
     return 0;
 }
