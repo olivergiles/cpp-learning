6c336434035b8c41f72fda4c6d9bdaae299f53bb
Oliver Giles <ogiles@email.com>
2022-04-16 18:24:57 +0100

    prod of two files

---
+++ prod-two.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+  std::cout << "Enter two numbers to get the product" << std::endl;
+  int v1 = 0, v2 = 0;
+  std::cin >> v1 >> v2;
+  std::cout << "The product of " << v1
+            << " and " << v2 << std::endl;
+  std::cout << "Is" << v1 * v2 << std::endl;
+}
