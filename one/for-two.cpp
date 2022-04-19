d1d01a199e4b65d29b7d9215126e3b28ccee1ada
Oliver Giles <ogiles@email.com>
2022-04-19 09:07:37 +0100

    for two numbers with for instead of while

---
+++ one/for-two.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  int val1=0, val2=0, sum=0;
+  std::cout << "Enter two digits to get the sum of integers between them"
+            << std::endl;
+  std::cin >> val1 >> val2;
+  for (int i = val1; i<=val2; ++i)
+    sum += i;
+  std::cout << "The sum between them is " << sum;
+}
