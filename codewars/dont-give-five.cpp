7db39b1bc81e54512b034bce2a565ba648dc22a7
Oliver Giles <ogiles@email.com>
2022-05-08 18:01:59 +0100

    read better

--- codewars/dont-give-five.cpp
+++ codewars/dont-give-five.cpp
@@ -4,6 +4,7 @@ int dontGiveMeFive(int start, int end)
 {
   int count = 0;
   for (start; start != end + 1; start++){
+    std::cout << start << "\n";
     if (start % 5){
       count++;
     }
@@ -14,5 +15,6 @@ int main()
 {
   std::cout << dontGiveMeFive(1,9) << "\n";
   std::cout << dontGiveMeFive(4, 17);
+  std::cout << dontGiveMeFive(-8, 2);
   return 0;
 }
